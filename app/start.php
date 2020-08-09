c342f14343a12d4de37cb35471ed38e1c91d10af
Mitchell Barry <mitch.barry@gmail.com>
2020-08-09 09:59:15 -0500

    Clean up start mysql sections

--- app/start.php
+++ app/start.php
@@ -3,7 +3,18 @@
 	require_once 'mysql.php';
 	echo "truncating table..........";
 	$sql = "TRUNCATE TABLE restaurant";
-	$result = mysql_query($sql) or die("failed<br /><br />" . mysql_error());
+
+	// 2020: needed slight refactor
+	// $result = mysql_query($sql) or die("failed<br /><br />" . mysql_error());
+	
+	$stmt = $db->prepare($sql);
+	try {
+		$stmt->execute();
+	} catch (Exception $e) {
+		echo 'Exception -> ';
+		var_dump($e->getMessage());
+	}
+	
 	echo nl2br("done\n");
 	echo "updating list.............";
     $run = "./restaurant.pl";

00e3fa415ba1f5da447ab96f326f6e6db51cb964
Mitchell Barry <mitch.barry@gmail.com>
2020-08-09 09:26:29 -0500

    Clean up override mysql sections

--- app/override.php
+++ app/override.php
@@ -1,33 +1,57 @@
 <?php
-	if($_GET['pass']!="oursecret")
-		header("Location: index.php");
- 	
-	require_once("mysql.php");	
-	if($_GET['action']=="override")
-	{
-		$restaurant = $_POST['place'];
-		$pass = md5("oursecret");
-		mysql_query("DELETE FROM restaurant WHERE '1'='1'");
-		mysql_query("INSERT INTO restaurant (name,pin) VALUES ('$restaurant','$pass')") or die(mysql_error());
+if ($_GET['pass'] != "oursecret")
+	header("Location: index.php");
+
+require_once("mysql.php");
+
+if ($_GET['action'] == "override") {
+	$restaurant = $_POST['place'];
+	$pass = md5("oursecret");
+
+	// 2020: needed slight refactor
+	// mysql_query("DELETE FROM restaurant WHERE '1'='1'");
+	// mysql_query("INSERT INTO restaurant (name,pin) VALUES ('$restaurant','$pass')") or die(mysql_error());
+
+	$query = "DELETE FROM restaurant WHERE 1=1";
+	$stmt = $db->prepare($query);
+	$stmt->execute();
+
+	$query = "INSERT INTO restaurant (name,pin) VALUES (?,?)";
+	$stmt = $db->prepare($query);
+	try {
+		$stmt->execute([$choice, $pin]);
+	} catch (Exception $e) {
+		echo 'Exception -> ';
+		var_dump($e->getMessage());
 	}
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xlm:lang="en" lang="en">
-	<head>
-		<title>Override</title>
-	</head>
-	<body>
-		<table align="center" width="600px" rules="none">
-			<form action="override.php?action=override&pass=oursecret" method="post">
+
+<head>
+	<title>Override</title>
+</head>
+
+<body>
+	<table align="center" width="600px" rules="none">
+		<form action="override.php?action=override&pass=oursecret" method="post">
 			<?php
-				if($_GET['action']=="override")
-					echo "<tr><td>OVERRIDE COMPLETE</td></tr>";
+			if ($_GET['action'] == "override")
+				echo "<tr><td>OVERRIDE COMPLETE</td></tr>";
 			?>
-			<tr><td>Override the Votes</td></tr>
-			<tr><td>Restaurant: <input name="place" type="text" maxlength="75"/></td><tr>
-			<tr><td><input type="submit" value="Override"/></td></tr>
-			</form>
-		</table>
-	</body>
+			<tr>
+				<td>Override the Votes</td>
+			</tr>
+			<tr>
+				<td>Restaurant: <input name="place" type="text" maxlength="75" /></td>
+			<tr>
+			<tr>
+				<td><input type="submit" value="Override" /></td>
+			</tr>
+		</form>
+	</table>
+</body>
+
 </html>

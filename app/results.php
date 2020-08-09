da9b3ad2a018518c47201ff62cd3d128201d3d03
Mitchell Barry <mitch.barry@gmail.com>
2020-08-09 09:50:24 -0500

    Cleanup results mysql sections

--- app/results.php
+++ app/results.php
@@ -1,43 +1,55 @@
 <?php
-        // connect to the database
-        include('mysql.php');
-        $sql = "SELECT DISTINCT name FROM restaurant";
-        $result = mysql_query($sql);
-	$results = "";
-	$i = 0;
-	$arr = array(0 => array("name" => "", "num" => 0),
-		 1 => array("name" => "", "num" => 0), 
-		2 => array("name" => "", "num" => 0));
-        while($row = mysql_fetch_assoc($result))
-        {
-                $sql2 = "SELECT * FROM restaurant WHERE name='" . 
-		mysql_real_escape_string($row['name']) . "'";
-                $result2 = mysql_query($sql2);
-                $num = mysql_num_rows($result2);
-		$arr[$i]["name"] = $row['name'];
-		$arr[$i]["num"] = $num;
-                $results .= $row['name'] . " : " . $num . "\n";
-		$i++;
-        }
-	$max = 0;
-	for($i = 0; $i < 3; $i++)
-	{
-		if($arr[$i]["num"] > $max && $arr[$i]["name"] != "")
-		{
-			$max = $arr[$i]["num"];
-			$winner = $arr[$i]["name"];
-			print nl2br("Winner is currently : $winner\n");
-		}
+// connect to the database
+include('mysql.php');
+
+// 2020: needed slight refactor
+// $sql = "SELECT DISTINCT name FROM restaurant";
+// $result = mysql_query($sql);
+
+$sql = "SELECT DISTINCT name FROM restaurant";
+$result = $db->query($sql);
+
+$results = "";
+$i = 0;
+$arr = array(
+	0 => array("name" => "", "num" => 0),
+	1 => array("name" => "", "num" => 0),
+	2 => array("name" => "", "num" => 0)
+);
+// 2020: needed slight refactor
+// while ($row = mysql_fetch_assoc($result)) {
+while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+	// 2020: needed slight refactor
+	// $sql2 = "SELECT * FROM restaurant WHERE name='" .
+	// 	  mysql_real_escape_string($row['name']) . "'";
+	// $result2 = mysql_query($sql2);
+	// $num = mysql_num_rows($result2);
+
+	$sql2 = "SELECT * FROM restaurant WHERE name=" . $db->quote($row['name']);
+	$result3 = $db->query($sql2);
+	$num = $result2->rowCount();
+
+	$arr[$i]["name"] = $row['name'];
+	$arr[$i]["num"] = $num;
+	$results .= $row['name'] . " : " . $num . "\n";
+	$i++;
+}
+$max = 0;
+for ($i = 0; $i < 3; $i++) {
+	if ($arr[$i]["num"] > $max && $arr[$i]["name"] != "") {
+		$max = $arr[$i]["num"];
+		$winner = $arr[$i]["name"];
+		print nl2br("Winner is currently : $winner\n");
 	}
-	$days = 7;
-	$thismonth = date("M", time());
-        $nextmonth = date("M", time()+($days*24*60*60));
-	$type = "Weekly";
-	if ($thismonth != $nextmonth)
-	{
-	    $type = "Monthly";
-	}
-	$mesg = "
+}
+$days = 7;
+$thismonth = date("M", time());
+$nextmonth = date("M", time() + ($days * 24 * 60 * 60));
+$type = "Weekly";
+if ($thismonth != $nextmonth) {
+	$type = "Monthly";
+}
+$mesg = "
 <html>
     <body style=\"font-family:Corbel,Tahoma,Arial;\">
     <div style=\"padding:15px;\">
@@ -46,35 +58,38 @@
     </div>
     </body>
 </html>";
-	$headers ="From:$type Lunch Notifier <lunch@mitchbarry.com>" . "\r\n";
-	$headers .= "Content-type: text/html\r\n";
-	$headers .= 'Cc: ';
-    if ( $thismonth == $nextmonth )
-	{
-		$sql = "SELECT email, frequency FROM users WHERE frequency='W'";
-		$headers .= get_emails($sql);
-	}
-    else
-	{
-		$sql = "SELECT email, frequency FROM users WHERE frequency='W' OR frequency='M'";
-		$headers .= get_emails($sql);
-	}
-	$subject = "Lunch this week: $winner";
-    mail("lunch@mitchbarry.com", $subject, $mesg, $headers);
-	echo nl2br($mesg);
+$headers = "From:$type Lunch Notifier <lunch@mg.mitchbarry.com>" . "\r\n";
+$headers .= "Content-type: text/html\r\n";
+$headers .= 'Cc: ';
+if ($thismonth == $nextmonth) {
+	$sql = "SELECT email, frequency FROM users WHERE frequency='W'";
+	$headers .= get_emails($sql);
+} else {
+	$sql = "SELECT email, frequency FROM users WHERE frequency='W' OR frequency='M'";
+	$headers .= get_emails($sql);
+}
+$subject = "Lunch this week: $winner";
+mail("lunch@mg.mitchbarry.com", $subject, $mesg, $headers);
+echo nl2br($mesg);
 
-	function get_emails($sql)
+function get_emails($sql)
+{
+	// 2020: needed slight refactor
+	// $result = mysql_query($sql) or die(mysql_error());
+	// while ($data = mysql_fetch_assoc($result)) {
+
+	global $db;
+	$emls = '';
+
+	$result = $db->query($sql);
+	while($data = $result->fetch(PDO::FETCH_ASSOC))
 	{
-		$result = mysql_query($sql) or die(mysql_error());
-		while($data = mysql_fetch_assoc($result))
-		{
-			if($emls != ''){
-				$emls .= ", ";
-			}
-			$emls .= $data['email'];
+		if ($emls != '') {
+			$emls .= ", ";
 		}
-		echo "<br />sent to: $emls<br />";
-		$emls .= "\r\n";
-		return $emls;
+		$emls .= $data['email'];
 	}
-?>
+	echo "<br />sent to: $emls<br />";
+	$emls .= "\r\n";
+	return $emls;
+}
